37412a96016bd9a95c2623bc885047e85b63e262
zezutom <zezulatomas@gmail.com>
2015-07-22 23:28:03 +0200

    Code cleanup

--- src/main/scala/org/zezutom/planner/exercises/DataTask.scala
+++
@@ -1,6 +0,0 @@
-package org.zezutom.planner.exercises
-
-trait DataTask[T] extends Serializable {
-
-  def execute(repo:Repository): T
-}
--- src/main/scala/org/zezutom/planner/exercises/RecordCounter.scala
+++
@@ -1,6 +0,0 @@
-package org.zezutom.planner.exercises
-
-class RecordCounter extends DataTask[Long] {
-
-  override def execute(repo: Repository): Long = repo.dataFrame.count()
-}
